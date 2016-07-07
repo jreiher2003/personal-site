5c0f4faeb9e6cfddb500a3e0c8619a9378950268
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-07 23:13:25 +0000

    Put QUOTE var in route function

--- personalsite/app/views.py
+++ personalsite/app/views.py
@@ -6,13 +6,14 @@ import requests
 
 a = {"I want to put a ding in the universe.": "Steve Jobs", "As we express our gratitude, we must never forget that the highest appreciation is not to utter words, but to live by them.": "John F. Kennedy", "I know where I'm going and I know the truth, and I don't have to be what you want me to be. I'm free to be what I want.": "Muhammand Ali", "Age is whatever you think it is. You are as old as you think you are.": "Muhammand Ali", "Always be yourself, express yourself, have faith in yourself, do not go out and look for a successful personality and duplicate it.": "Bruce Lee", "The starting point of all achievement is desire.": "Napoleon Hill", "Success consists of going from failure to failure without loss of enthusiasm.": "Winston Churchill"}
 
-QUOTE = random.choice(list(a.items()))
 @app.route('/')
 def hello_world():
+    QUOTE = random.choice(list(a.items()))
     return render_template("index.html", QUOTE=QUOTE)
 
 @app.route("/projects")
 def projects():
+    QUOTE = random.choice(list(a.items()))
     headers = {"Content-Type": "application/json", "User-Agent": "jreiher2003"}
     puppy = requests.get("https://api.github.com/repos/jreiher2003/Puppy-Adoption", headers=headers).json()
     portfolio = requests.get("https://api.github.com/repos/jreiher2003/Jeff-Portfolio", headers=headers).json()
