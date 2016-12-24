753815c4c6d349d12b206794bda9b3c7a4a57e45
Jeff Reiher <jreiher2003@yahoo.com>
2016-12-24 17:52:06 +0000

    Add user weather

--- personalsite/app/views.py
+++ personalsite/app/views.py
@@ -1,14 +1,32 @@
+import os
 import random
 from datetime import datetime
 import pytz
 import requests
 from app import app
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, jsonify, request 
+import config 
 from config import BaseConfig 
 
 def header():
     return {"Content-Type": "application/json", "User-Agent": "jreiher2003"}
 headers = header()
+
+def get_my_ip():
+    return request.remote_addr
+
+def find_my_loc():
+    user = get_my_ip()
+    if os.environ["APP_SETTINGS"] == 'config.DevelopmentConfig':
+        return requests.get("http://ipinfo.io/73.55.103.114").json()
+    else:
+        return requests.get("http://ipinfo.io/" + user).json()
+
+def find_lon_lat():
+    user_loc = find_my_loc()
+    lon = user_loc['loc'].split(",")[1]
+    lat = user_loc['loc'].split(",")[0]
+    return lat, lon
 
 def rand_pic(len_object):
     return random.randint(1,len_object+1)
@@ -21,9 +39,20 @@ def profiles():
     return requests.get("https://api.github.com/users/jreiher2003", headers=headers).json()
 profile = profiles()
 
+def find_current_weather(params):
+    APIKEY = "APPID="+BaseConfig.OPEN_WEATHER_MAP
+    URL = "http://api.openweathermap.org/data/2.5/"
+    cur = params
+    m = URL + cur + APIKEY
+    # print m
+    return requests.get(m, headers=headers).json()
+
+def find_user_weather(lat, lon):
+    return find_current_weather("weather?lat=%s&lon=%s&units=imperial&" % (lat,lon))
+
 def find_sunset_sunrise():
     local_tz = pytz.timezone("US/Eastern")
-    weather = find_current_weather()
+    weather = find_current_weather("weather?id=4154465,us&units=imperial&")
     s = weather['sys']['sunset']
     r = weather['sys']['sunrise']
     ss = datetime.fromtimestamp(s).replace(tzinfo=pytz.utc)
@@ -32,18 +61,14 @@ def find_sunset_sunrise():
     sunrise = rr.astimezone(pytz.timezone('US/Eastern'))
     return sunrise,sunset
 
-def find_current_weather():
-    APIKEY = "APPID="+BaseConfig.OPEN_WEATHER_MAP
-    URL = "http://api.openweathermap.org/data/2.5/"
-    cur = "weather?id=4154465,us&units=imperial&"
-    m = URL + cur + APIKEY
-    return requests.get(m, headers=headers).json()
-
 @app.route('/')
 def hello_world():
+    user_loc = find_my_loc()
+    lat, lon = find_lon_lat()
+    user_weather = find_user_weather(lat,lon)
     now_utc = datetime.now(pytz.timezone('UTC'))
     dt = now_utc.astimezone(pytz.timezone('US/Eastern'))
-    weather = find_current_weather()
+    weather = find_current_weather("weather?id=4154465,us&units=imperial&")
     sunrise,sunset = find_sunset_sunrise()   
     quote = quotes()
     return render_template(
@@ -53,7 +78,9 @@ def hello_world():
         quote=quote,
         weather=weather,
         sunrise=sunrise,
-        sunset=sunset
+        sunset=sunset,
+        user_loc=user_loc,
+        user_weather=user_weather
         )
 
 @app.route("/projects")
